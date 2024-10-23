9beae3d08b5cfcf067f44b02e0450cb29861102a
John Ihor Campagna <johncampagna2@gmail.com>
2024-10-23 12:13:48 -0400

    next update

---
+++ viz.R
@@ -0,0 +1,20 @@
+install.packages("dplyr")
+
+library(dplyr)
+
+install.packages("pillar")
+install.packages("Rcpp")
+install.packages("dplyr")
+
+setwd("C:/Users/maxim/Documents/Data")
+st <-  read.csv('Strava_clean.csv')
+attach(st)
+
+#Subsetting to remove outliers
+st2 <-  select(filter(st, st$Mile > 4 & st$Per.Mile.Pace < 9 ), -st$Average.Temperature,-st$Max.Temperature)
+st2 <-  filter(st, st$Mile > 4 & st$Per.Mile.Pace < 9 )
+
+
+summary(lm(st2$Per.Mile.Pace~st2$Mile+st2$Average.Heart.Rate+st2$Average.Grade+st2$Elevation.Gain))
+
+summary(lm(st2$Per.Mile.Pace~st2$Activity.ID))
